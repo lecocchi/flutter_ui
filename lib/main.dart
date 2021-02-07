fa5b9199c89bf9233dca94fa01b4d943223f918e
= <leju1712@gmail.com>
2021-02-07 13:16:16 -0300

    Create calculator interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/calculator_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,16 +8,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: CalculatorPage(),
     );
   }
 }
